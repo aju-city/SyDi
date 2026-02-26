7099b6ae61906a3c4153f027cc89dc94c2d6e7c7
aju-city <Arjun.Bector@city.ac.uk>
2026-02-26 11:04:43 +0000

    Re-generated Java code after fixing spelling error

---
+++ 1st Deliverable/VPP/Class Diagram/Java Code/IPOS_PU/CAtoPU.java
@@ -0,0 +1,16 @@
+package IPOS_PU;
+
+public class CAtoPU implements I_CAtoPU {
+
+	public void updateOnlineOrderStatus(String aOrderld, Object aStatus) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean sendEmail(EmailMessage aMsg) {
+		throw new UnsupportedOperationException();
+	}
+
+	public CAtoPU() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ 1st Deliverable/VPP/Class Diagram/Java Code/IPOS_PU/SAtoPU.java
@@ -0,0 +1,12 @@
+package IPOS_PU;
+
+public class SAtoPU implements I_SAtoPU {
+
+	public boolean sendEmail(EmailMessage aMsg) {
+		throw new UnsupportedOperationException();
+	}
+
+	public SAtoPU() {
+		throw new UnsupportedOperationException();
+	}
+}
